36ad6f19b374b80917059bf83cd279b15b75620d
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-30 21:26:06 +0600

    your comment

---
+++ variable-name.js
@@ -0,0 +1,27 @@
+
+var name = "Hero Alon";
+
+
+// reserve keyword not allowed ...amra niche dektechi je ekta variable ditechi b true name diye,,,sekhane error dejhabe ,,,karon variable e kichu key word ache jeigula eikhabe bebohar kora jabe na ....
+
+var true = "sakib pass kore nai";
+
+
+// double name e is not allowed 
+var nayok name = "tom and jerry ";
+
+
+var my-office - adress = 'huuu haaaa';
+var my_office_adress = 'huuu haaaa';
+var myOfficeAdress = 'huuu haaaa';
+var myofficeadress = 'huuu haaaa';
+
+var pie = 3.1416;
+var price = 25;
+
+
+
+
+
+
+
--- variable.js
+++ variable.js
@@ -1,10 +1,18 @@
 var price = 21;
-
 var age = 25;
-
 var tempareture = 36;
-
 var month = 06;
-
 var year = 2022;
+var family = 6;
+
+var name = "sodor uddin";
+
+var adress = "vooter barir add";
+
+var school = "green school";
+
+var pass = true;
+
+var suscribe = false;
+
 
